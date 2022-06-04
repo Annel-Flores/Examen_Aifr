215692dd36d3483dba84bdb9a287691d01c94c79
Annel Flores <bluegames14777@gmail.com>
2022-06-03 20:32:18 -0500

    Agregar archivos de proyecto.

---
+++ Examen_Aifr/ICalcular.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Examen_Aifr
+{
+    interface ICalcular
+    {
+        public void Calcular();
+    }
+}
---
+++ Examen_Aifr/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Examen_Aifr;
+namespace Examen_Aifr
+{
+    public class Program
+    {
+        private static int length;
+
+        public static int Length { get => length; set => length = value; }
+
+        private static void Main(string[] args)
+        {
+            Primaria primaria = new Primaria("Annel", "Flores", " Ruperto Mena", "1351214497", 500);
+            Secundaria secundaria = new Secundaria("Annel", "Flores", " Cinco de Junio ", "1351214497", 6, 200);
+            Universitarios U = new Universitarios("Annel", "Flores", " Eloy Alfaro de Manabí", "1351214497", 300, 20);
+            for (int i = Length + 1; i >= 0; i++)
+            {
+                primaria.ImprimirDatos();
+                primaria.Calcular();
+                Console.WriteLine("");
+                secundaria.Calcular();
+                Console.WriteLine("");
+                U.ImprimirDatos();
+                U.Calcular();
+            }
+        }
+    }
+}
